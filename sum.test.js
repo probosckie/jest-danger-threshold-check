a7a65dc064da5c2ecb77a6791f9264f8897afa9b
prabhasjoshi <prabhas.joshi@razorpay.com>
2021-12-27 18:38:36 +0530

    fix

--- sum.test.js
+++ sum.test.js
@@ -2,6 +2,10 @@ import {add, add2} from './sum';
 
 describe('Test Suite for Sum', () => {
   test('should return sum of 2 numbers', () => {
-    expect(add(1,2)).toEqual(3)    
+    expect(add(1,2)).toEqual(3)
+  });
+
+  test('should return sum of 2 other numbers', () => {
+    expect(add(2,2)).toEqual(4)
   });
 });
