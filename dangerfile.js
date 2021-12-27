95e68233fa553839a8210da96200f7fed711cadb
prabhasjoshi <prabhas.joshi@razorpay.com>
2021-12-27 22:35:47 +0530

    fix

--- dangerfile.js
+++ dangerfile.js
@@ -1,9 +1,9 @@
 import { message, danger } from 'danger';
-import jest from 'danger-plugin-jest';
-//import { codeCoverage } from 'danger-plugin-code-coverage';
+//import jest from 'danger-plugin-jest';
+import { codeCoverage } from 'danger-plugin-code-coverage';
 
 const modifiedMD = danger.git.modified_files.join('- ');
 message('Changed Files in this PR: \n - ' + modifiedMD);
 
-jest();
-//codeCoverage();
+//jest();
+codeCoverage();
