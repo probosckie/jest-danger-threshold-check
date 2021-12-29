7149a6be916ace03a5082b8cdbc402e4ec395527
prabhasjoshi <prabhas.joshi@razorpay.com>
2021-12-29 13:00:41 +0530

    fix

--- src/sum.js
+++ src/sum.js
@@ -1,12 +1,13 @@
-import { subtract } from './subtract';
-const add = (x, y) => {
-  return x + y;
-};
+//import { subtract } from './subtract';
+const d = 3;
+const d1 = true;
 
 const add2 = (x, y, z) => {
   return x + y + z;
 };
 
-const d = 2;
-const d1 = true;
-export { add, add2 };
+const add = (x, y) => {
+  return x + y;
+};
+
+export { add2, add };
