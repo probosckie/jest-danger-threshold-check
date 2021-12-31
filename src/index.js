3f78ffec4a8e7c841351fc9c63ef46885aac14ee
prabhasjoshi <prabhas.joshi@razorpay.com>
2021-12-31 16:08:02 +0530

    fix

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+const title = 'React with Webpack and Babel';
+
+ReactDOM.render(<App title={title} />, document.getElementById('app'));
