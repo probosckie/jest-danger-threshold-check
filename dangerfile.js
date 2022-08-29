d04435f4ac23502d5d010dbe25952bad69a5d42f
Prabhas Joshi <joshi.prabhas@gmail.com>
2022-08-29 16:47:17 +0530

    Update dangerfile.js

--- dangerfile.js
+++ dangerfile.js
@@ -1,5 +1,5 @@
 import { danger, schedule, warn, fail, markdown } from 'danger';
-const testCoverageUtil = require('./src/js/testCoverageCheck');
+const testCoverageUtil = require('./src/testCoverageCheck');
 
 
 schedule(() => testCoverageUtil.checkTestCoverage({ danger, fail, markdown }));
